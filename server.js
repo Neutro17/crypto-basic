6cfadf4f0738925db15785a5def650ba117b2923
Neutro17 <geovannesousa16@hotmail.com>
2022-08-21 11:38:17 -0300

    web e mobile

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var cors = require('cors')
+var app = express();
+
+app.use(cors)
+
+const express = require("express");
+const app = express();
+ 
+
+
+app.listen(3000, ()=>{
+    console.log('Funciona');
+})
